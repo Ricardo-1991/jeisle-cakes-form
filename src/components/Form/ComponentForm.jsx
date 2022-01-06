4b8ad3d5b20dc6e8190e8120ca32e24d3bc7b295
Ricardo <pauloricardo_wow@hotmail.com>
2022-01-05 23:11:23 -0300

    excluindo segundo state para recheios adicionais

--- src/components/Form/ComponentForm.jsx
+++ src/components/Form/ComponentForm.jsx
@@ -4,42 +4,6 @@ import { Formik, Field, Form} from 'formik'
 import avatar from '../../images/jeysle-menu.jpeg'
 
 
-/*Vê se essa lógica te ajuda:
-A ideia é que se o recheio selecionado não existir na variável de "recheiosAdicionais", ele coloca o preço como 0 na conta. */
-
-
-/*
-  Ela primeiro pega o valor do bolo, e salva na variável valorDoBolo.
-No estado do bolo bolo vai ter uma string contendo o tamanho do bolo, ex: 15. 
-Supondo que seja "15", ali na declaração da constante valorDoBolo eu defino que ela é tamanhosDeBolo["15"]. 
-
-tamanhosDeBolo["15"] tem o valor 800, que é o que tá lá no objeto.
-
-Depois ela faz a mesma coisa pros recheios 1 e 2. E no final soma. 
-
-
-Exato. E se no objeto de "recheiosAdicionais" não existir o recheio que a pessoa adicionou, por exemplo: recheiosAdicionais["15"]["banana"], a variável fica igual a 0, e não interfere no preço final.
-
-Ali na função eu uso um operador ternário pra isso.
-
-const valorDoRecheio1 = recheiosAdicionais["banana"] ? recheiosAdicionais["banana"]["15"] : 0
-} 
-
-const [bolo, setBolo] = useState(); //setBolo("15");
-const [recheio1, setRecheio1] = useState(); //setRecheio1({ sabor: "banana", tamanho: "15" });
-const [recheio2, setRecheio2] = useState(); //setRecheio2({ sabor: "banana", tamanho: "15" });
-
-const calcularValorFinal = () => {
-  const valorDoBolo = tamanhosDeBolo[ bolo ] || 0;
-  const valorDoRecheio1 = recheiosAdicionais[ recheio1.sabor ] ? recheiosAdicionais[ recheio1.sabor ][ recheio1.tamanho ] : 0;
-  const valorDoRecheio2 = recheiosAdicionais[ recheio2.sabor ] ? recheiosAdicionais[ recheio2.sabor ][ recheio2.tamanho ] : 0;
-
-  return valorDoBolo + recheioAdicional1 + recheioAdicional2;
-}
-
-*/
-
-
 const tamanhosDeBolo = {
   "15": 110, 
   "20": 160,
@@ -115,36 +79,9 @@ export const ComponentForm = () => {
 
   const [filling, setFilling] = useState([null,null]);
 
-  const [aditionalFilling, setAditionalFilling] = useState([null, null])
-
-  // let [price, setPrice] = useState(0) // total do pedido.
-
-
   //Estado diâmetros do bolo
   const changeDiameter = (evt) => {
-    let diameter = evt.target.value
-    setDiameter(evt.target.value)
-   
-    // switch (diameter) {
-    //   case '15':
-    //      setPrice(110)
-    //     break;
-
-    //     case '20':
-    //      setPrice(160)
-    //     break;
-
-    //     case '25':
-    //      setPrice(220)
-    //     break;
-
-    //     case '30':
-    //      setPrice(280)
-    //     break;
-    //   default:
-    //     break;
-    // }
-        
+    setDiameter(evt.target.value)   
   }
 
   //Estado Massas de Bolo
@@ -158,9 +95,6 @@ export const ComponentForm = () => {
   
   // Estado Recheios
   const changeFilling = evt =>{
-    let fillingType = evt.target.name;
-
-    if(fillingType == 'recheio'){
       //Verifica se o valor já existe nos dois selecionados
       if(filling.find(val => val == evt.target.value)){
         // Caso existe, filtra tudo que não seja o valor selecionado ( utilizado para desmarcar )
@@ -169,55 +103,25 @@ export const ComponentForm = () => {
         // Caso seja um valor novo, move  o ultimo valor para o inicio e adiciona o valor novo no fim da estrutura
         setFilling([filling[1], evt.target.value])
       }
-
-      //filling = chega os dois recheios normais no Array. Exemplo: [Amendoim, Ninho]   
-
-    } else {
-       //Verifica se o valor já existe nos dois selecionados
-       if(aditionalFilling.find(val => val == evt.target.value)){
-        // Caso existe, filtra tudo que não seja o valor selecionado ( utilizado para desmarcar )
-        setAditionalFilling(aditionalFilling.filter( val => val != evt.target.value))
-      } else {
-        // Caso seja um valor novo, move  o ultimo valor para o inicio e adiciona o valor novo no fim da estrutura
-        setAditionalFilling([aditionalFilling[1], evt.target.value])
-      }  
-    }
   }
 
   function totalPriceOfCake (){
-    let cakeValue = tamanhosDeBolo[diameterState]
-    const valorDosRecheios = aditionalFilling.reduce((acumulator, recheio) => {
+    const cakeValue = tamanhosDeBolo[diameterState]
+    const valorDosRecheios = filling.reduce((acumulator, recheio) => {
       const valorDoRecheioAtual = recheiosAdicionais[recheio] ? recheiosAdicionais[recheio][diameterState] : 0
       return Number(acumulator) + Number(valorDoRecheioAtual)
     }, 0)
     return Number(cakeValue) + Number(valorDosRecheios)
   }
   
-  let total = totalPriceOfCake()
-  console.log(total)
+  const total = totalPriceOfCake()
  
-  
- 
-
-  // totalPriceOfCake()
-
-  // Object.keys(recheiosAdicionais).map(function(recheiosAdd, index, arr){
-  //   console.log(index)
-  //   console.log(recheiosAdd)
-  //   console.log(arr)
-  // }) 
- 
-
-
-
-
 
   function handleSubmit () {
     location.href = `https://api.whatsapp.com/send?phone=5574999990520&text=
     _Tamanho_: *${diameterState == null? `` : `${diameterState}  cm`}* %0a_Massa do bolo_: *${ batterState[0] == null ? `${batterState.slice(1)} ` : `${batterState.join(' e ')}`}* 
-    %0a_Recheios_: *${filling[0] == null? `${filling.slice(1)}` : `${filling.join(' e ')}`}* 
-    ${aditionalFilling[0,1] == null? `%0a_Recheios Adicionais_: *Nenhum Recheio Adicional foi escolhido*` : `%0a_Recheios Adicionais_: *${aditionalFilling[0] == null? `${aditionalFilling.slice(1)}` : `${aditionalFilling.join(' e ')}`}* ` }
-    Valor total do bolo: R$${price}`
+    %0a_Recheios_: *${filling[0] == null? `${filling.slice(1)}` : `${filling.join(' e ')}`}*
+    Valor total do bolo: R$${total}`
  }
 
   return (
@@ -450,7 +354,7 @@ export const ComponentForm = () => {
                 <h2>Recheios com valor adicional</h2>
                 <div className='containerLabel'>
                   <label>
-                    <Field type="checkbox" name="recheioAdd" value="Abacaxi" onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Abacaxi')  ? true : false } />-
+                    <Field type="checkbox" name="recheioAdd" value="Abacaxi" onChange={changeFilling} checked={filling.find( val => val == 'Abacaxi')  ? true : false } />-
                     Abacaxi
                   </label>
                 </div>
@@ -461,7 +365,7 @@ export const ComponentForm = () => {
                       type="checkbox"
                       name="recheioAdd"
                       value="Ameixa"
-                      onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Ameixa')  ? true : false }
+                      onChange={changeFilling} checked={filling.find( val => val == 'Ameixa')  ? true : false }
                     />
                     - Ameixa
                   </label>
@@ -473,7 +377,7 @@ export const ComponentForm = () => {
                       type="checkbox"
                       name="recheioAdd"
                       value="Limão Siciliano"
-                      onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Limão Siciliano')  ? true : false }
+                      onChange={changeFilling} checked={filling.find( val => val == 'Limão Siciliano')  ? true : false }
                     />
                     - Limão Siciliano
                   </label>
@@ -481,35 +385,35 @@ export const ComponentForm = () => {
 
                 <div className='containerLabel'>
                   <label>
-                    <Field type="checkbox" name="recheioAdd" value="Maracujá" onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Maracujá')  ? true : false } />-
+                    <Field type="checkbox" name="recheioAdd" value="Maracujá" onChange={changeFilling} checked={filling.find( val => val == 'Maracujá')  ? true : false } />-
                     Maracujá
                   </label>
                 </div>
 
                 <div className='containerLabel'>
                   <label>
-                    <Field type="checkbox" name="recheioAdd" value="Morango" onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Morango')  ? true : false } />-
+                    <Field type="checkbox" name="recheioAdd" value="Morango" onChange={changeFilling} checked={filling.find( val => val == 'Morango')  ? true : false } />-
                     Morango
                   </label>
                 </div>
 
                 <div className='containerLabel'>
                   <label>
-                    <Field type="checkbox" name="recheioAdd" value="Nozes" onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Nozes')  ? true : false } />-
+                    <Field type="checkbox" name="recheioAdd" value="Nozes" onChange={changeFilling} checked={filling.find( val => val == 'Nozes')  ? true : false } />-
                     Nozes
                   </label>
                 </div>
 
                 <div className='containerLabel'>
                   <label>
-                    <Field type="checkbox" name="recheioAdd" value="Ovomaltine" onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Ovomaltine')  ? true : false } />-
+                    <Field type="checkbox" name="recheioAdd" value="Ovomaltine" onChange={changeFilling} checked={filling.find( val => val == 'Ovomaltine')  ? true : false } />-
                     Ovomaltine
                   </label>
                 </div>
 
                 <div className='containerLabel'>
                   <label>
-                    <Field type="checkbox" name="recheioAdd" value="Oreo" onChange={changeFilling} checked={aditionalFilling.find( val => val == 'Oreo')  ? true : false } />- Oreo
+                    <Field type="checkbox" name="recheioAdd" value="Oreo" onChange={changeFilling} checked={filling.find( val => val == 'Oreo')  ? true : false } />- Oreo
                   </label>
                 </div>
               </section>
