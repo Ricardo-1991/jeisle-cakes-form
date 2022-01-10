c32eac142841aa6308e811f43921ccebf0b47881
Ricardo <pauloricardo_wow@hotmail.com>
2022-01-10 01:15:59 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import { useState } from 'react'
 import { ComponentForm } from './components/Form/ComponentForm'
 import { Header } from './components/Header/Header'
 import { Container } from './components/UIcontainer/Container'
-import Modal from 'react-modal'
 
-Modal.setAppElement('#root')
 function App() {
   return (
       <Container>
