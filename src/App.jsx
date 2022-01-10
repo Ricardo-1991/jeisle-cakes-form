80ccce61c1201406d518b3804cd9d7cc8d9f5808
Ricardo <pauloricardo_wow@hotmail.com>
2022-01-10 01:31:17 -0300

    renomeando para jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
-import { useState } from 'react'
 import { ComponentForm } from './components/Form/ComponentForm'
 import { Header } from './components/Header/Header'
 import { Container } from './components/UIcontainer/Container'
 
 function App() {
   return (
+    <>
       <Container>
         <Header />
         <ComponentForm />
       </Container>
+    </>
   )
 }
 
 export default App
+
