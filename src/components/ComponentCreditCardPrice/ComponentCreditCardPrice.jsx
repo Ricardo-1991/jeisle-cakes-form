afa41059e79c5ee1fc8aac80f18eef04a0437d50
Ricardo <pauloricardo_wow@hotmail.com>
2022-02-18 03:11:51 -0300

    ajuste

--- src/components/ComponentCreditCardPrice/ComponentCreditCardPrice.jsx
+++ src/components/ComponentCreditCardPrice/ComponentCreditCardPrice.jsx
@@ -8,6 +8,13 @@ export function ComponentCreditCardPrice({ states, formHandleInstallment }) {
     formHandleInstallment(evt.target.value)
   }
 
+  function formatPrice(price) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price)
+  }
+
   return (
     <>
       {states.paymentMethod == 'Credito' && (
@@ -24,34 +31,52 @@ export function ComponentCreditCardPrice({ states, formHandleInstallment }) {
               >
                 <option>Escolha uma opção</option>
                 <option value="1x">
-                  1x de R$ {states.installmentsPrice.totalPriceCreditCard['1x']}{' '}
-                  com juros de 2,9 % a.m total: R$
-                  {states.installmentsPrice.totalPriceCreditCard['1x']}
+                  1x de{' '}
+                  {formatPrice(states.installmentsPrice.installments['1x'])} com
+                  juros de 2,9 % a.m total:{' '}
+                  {formatPrice(
+                    states.installmentsPrice.totalPriceCreditCard['1x']
+                  )}
                 </option>
                 <option value="2x">
-                  2x de R$ {states.installmentsPrice.installments['2x']} com
-                  juros de 4,59 % a.m. total: R${' '}
-                  {states.installmentsPrice.totalPriceCreditCard['2x']}
+                  2x de{' '}
+                  {formatPrice(states.installmentsPrice.installments['2x'])} com
+                  juros de 4,59 % a.m. total:{' '}
+                  {formatPrice(
+                    states.installmentsPrice.totalPriceCreditCard['2x']
+                  )}
                 </option>
                 <option value="3x">
-                  3x de R$ {states.installmentsPrice.installments['3x']} com
-                  juros de 5,23 % a.m. total: R${' '}
-                  {states.installmentsPrice.totalPriceCreditCard['3x']}
+                  3x de{' '}
+                  {formatPrice(states.installmentsPrice.installments['3x'])} com
+                  juros de 5,23 % a.m. total:{' '}
+                  {formatPrice(
+                    states.installmentsPrice.totalPriceCreditCard['3x']
+                  )}
                 </option>
                 <option value="4x">
-                  4x de R$ {states.installmentsPrice.installments['4x']} com
-                  juros de 5,87 % a.m. total: R${' '}
-                  {states.installmentsPrice.totalPriceCreditCard['4x']}
+                  4x de{' '}
+                  {formatPrice(states.installmentsPrice.installments['4x'])} com
+                  juros de 5,87 % a.m. total:{' '}
+                  {formatPrice(
+                    states.installmentsPrice.totalPriceCreditCard['4x']
+                  )}
                 </option>
                 <option value="5x">
-                  5x de R$ {states.installmentsPrice.installments['5x']} com
-                  juros de 6,51 % a.m. total: R${' '}
-                  {states.installmentsPrice.totalPriceCreditCard['5x']}
+                  5x de{' '}
+                  {formatPrice(states.installmentsPrice.installments['5x'])} com
+                  juros de 6,51 % a.m. total:{' '}
+                  {formatPrice(
+                    states.installmentsPrice.totalPriceCreditCard['5x']
+                  )}
                 </option>
                 <option value="6x">
-                  6x de R$ {states.installmentsPrice.installments['6x']} com
-                  juros de 7,15 % a.m. total: R${' '}
-                  {states.installmentsPrice.totalPriceCreditCard['6x']}
+                  6x de{' '}
+                  {formatPrice(states.installmentsPrice.installments['6x'])} com
+                  juros de 7,15 % a.m. total:{' '}
+                  {formatPrice(
+                    states.installmentsPrice.totalPriceCreditCard['6x']
+                  )}
                 </option>
               </select>
             </label>
